73b6de313754c9f643fd3eb9648ea4266869b393
Egzon Arifi <egzon.r.arifi@gmail.com>
2026-05-14 11:11:21 +0200

    Release 1.1.0

--- lingua.rb
+++ lingua.rb
@@ -1,8 +1,8 @@
 class Lingua < Formula
   desc "Unified localization management tool for iOS & Android via Google Sheets"
   homepage "https://github.com/poviolabs/Lingua"
-  url "https://github.com/poviolabs/Lingua/releases/download/1.0.6/Lingua"
-  sha256 "827b5ab8c394c4e90e5d28bb9ac5835827228707a18ccc66d6c6844c831c59ca"
+  url "https://github.com/poviolabs/Lingua/releases/download/1.1.0/Lingua"
+  sha256 "ceb1aac6b0380cc3cbd68901a773e7ed946f833008ff470a375e482ea38da529"
   
   def install
     bin.install "Lingua"
